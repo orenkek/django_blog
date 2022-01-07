5be24b0dbd61f5faff90807d63c2f6f51bf245b4
anuarcheck <anuarcheck@gmail.com>
2022-01-07 18:46:27 +0500

    feat: Made Post output to home.html

--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'base/home.html')
+    posts = Post.objects.all()
+
+    context = {'posts': posts}
+
+    return render(request, 'base/home.html', context)
